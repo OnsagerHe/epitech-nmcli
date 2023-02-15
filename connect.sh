248f590ddc25952161a7662b81915c5669cee490
albert VALENTIN <avalentin@scaleway.com>
2023-02-15 15:51:47 +0100

    add: complete connection to network

--- connect.sh
+++ connect.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
 UUID_CONNECTION=$(uuidgen)
-INITIAL_FILE=IONIS-BETA.nmconnection
-TARGET_LOCATION=/etc/NetworkManager/system-connection
+INITIAL_FILE=IONIS.nmconnection
+TARGET_LOCATION=/etc/NetworkManager/system-connections
 FILE=/etc/NetworkManager/system-connections/IONIS.nmconnection
 
-if [[ -f "$FILE" ]]; then
-    echo "$FILE exists."
-fi
+function network_manager_exist() {
+	if [[ ! -f "$FILE" ]]; then
+    	echo "NetworkManager exist ?"
+    	exit 1
+	fi
+}
 
-echo $UUID_CONNECTION
+function set_credentials() {
+	read -p 'Username: ' uservar
+	read -sp 'Password: ' passvar
+	echo
 
-read -p 'Username: ' uservar
-read -sp 'Password: ' passvar
-echo
+	sed -i "s/USERNAME/$uservar/g" $INITIAL_FILE
+	sed -i "s/PASSWORD/$passvar/g" $INITIAL_FILE
+	sed -i "s/UUID/$UUID_CONNECTION/g" $INITIAL_FILE
+}
 
-sed -i "s/USERNAME/$uservar/g" $INITIAL_FILE
-sed -i "s/PASSWORD/$passvar/g" $INITIAL_FILE
-sed -i "s/UUID/$UUID_CONNECTION/g" $INITIAL_FILE
+function reload_network_manager() {
+	sudo cp $INITIAL_FILE $TARGET_LOCATION
 
-cp $INITIAL_FILE $TARGET_LOCATION
+	sudo systemctl restart NetworkManager
+	nmcli con up IONIS
+}
 
-sudo systemctl restart NetworkManager
-nmcli con up IONIS
+function main() {
+	network_manager_exist
+	set_credentials
+	reload_network_manager
+	exit 0
+}
 
+main
